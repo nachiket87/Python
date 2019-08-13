58cab8d6e5e351f3f636c86f03f092ad8cd98dbf
Nachiket Pusalkargit config --global user.email nachiket87@gmail.com <nachiketpusalkar@Nachikets-MacBook-Pro.local>
2019-08-13 17:26:48 -0400

    Initial Commit

---
+++ writingtofiles.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul  8 21:10:32 2019
+
+@author: nachiketpusalkar
+"""
+
+with open("test.txt", "w") as file1:
+   
+
+    print(file1.name)
+    file1.write("This is what I added")
+    
