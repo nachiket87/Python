f112b6c0c5687933ce275689b1a501a7efe6d38b
Nachiket Pusalkar <nachiket87@gmail.com>
2020-05-11 14:14:46 -0400

    tested

--- shuffle.py
+++ shuffle.py
@@ -12,11 +12,12 @@ def shuffle(the_list):
     last_index = len(the_list) - 1
 
     for index_to_be_chosen in range(0, len(the_list)-1):
-        random_index = get_random(index_to_be_chosen, last_index)
+        random_index = get_random(index_to_be_chosen, last_index) #pick a random index value
 
         
-        if random_index != index_to_be_chosen:
-            the_list[index_to_be_chosen], the_list[random_index] = the_list[random_index], the_list[index_to_be_chosen]
+        if random_index != index_to_be_chosen: #good to go as long as the random index wasn't the same.
+            the_list[index_to_be_chosen], the_list[random_index] = \
+            the_list[random_index], the_list[index_to_be_chosen] #swap the index number with random index
     return the_list
 
 print(shuffle([1,2,3,4,5]))
